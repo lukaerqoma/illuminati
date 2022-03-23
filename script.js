8bb4d1439640f939744903d5d7d844ccfe00c0aa
lukaerqoma <92222447+lukaerqoma@users.noreply.github.com>
2022-03-23 17:40:08 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//FAQ
+let summaryCollection = document.getElementsByTagName("summary");
+let signsCollection = document.getElementsByClassName("faq-open-icon");
+
+for (let i = 0; i < summaryCollection.length; i++) {
+  summaryCollection[i].onclick = function () {
+    if (signsCollection[i].innerHTML === "+")
+      signsCollection[i].innerHTML = "—";
+    else signsCollection[i].innerHTML = "+";
+  };
+}
+//products//
